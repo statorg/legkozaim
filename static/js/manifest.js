aea6fb2159e2f3a0c34296daebe914297639ba8c
statorg <ruslanshakir@gmail.com>
2018-03-20 07:29:44 +0300

    Add files via upload

--- static/js/manifest.js
+++ static/js/manifest.js
@@ -92,11 +92,11 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "/legkozaim/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=manifest.js.map?70af1ecbef4a66a004a7
+//# sourceMappingURL=manifest.js.map?728458ea5ccab6e5c9ba
